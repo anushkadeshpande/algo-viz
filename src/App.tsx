fbc93bf0f825ee637610d9f928280d81afb4070f
anushkadeshpande <anushkaa5000@gmail.com>
2024-10-13 03:45:34 +0530

    feat: Removed the unnecessary higher order component and passed the array to the BubbleSort component directly

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
 import BubbleSort from './BubbleSort'
-import Canvas from './Canvas'
+// import BubbleSort from './BubbleSort'
+// import Canvas from './Canvas'
+// import Array from './components/NumberArray'
+
+// const Array1 = Array({arr: [1, 2, 3, 4, 5, 100, 3, 4, 5, 1, 3]})
 
 function App() {
   return (
-    <>
-    <BubbleSort />
+    <div>
+    {/* <Array arr={[1, 2, 3, 4, 5, 100, 3, 4, 5, 1, 3]}/> */}
+    {/* {bubbleSort()} */}
+    <BubbleSort arr={[1, 2, 3, 4, 5, 100, 3, 4, 5, 1, 3]} />
     {/* <Canvas /> */}
-    </>
+    </div>
   )
 }
 
