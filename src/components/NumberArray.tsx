06909ded1c8dd0dd2a3adee1eadcd84c397c1037
anushkadeshpande <anushkaa5000@gmail.com>
2024-10-13 16:06:26 +0530

    feat: Added swap animation to show the sorting of the array step by step

--- src/components/NumberArray.tsx
+++ src/components/NumberArray.tsx
@@ -1,43 +1,71 @@
-import React, { useEffect, useState } from "react";
-import './NumberArray.css'
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import "./NumberArray.css";
+import { motion } from "framer-motion";
 
-const NumberArray = ({ arr, algorithm }: { arr: Array<number>; algorithm: any }) => {
+const NumberArray = ({
+  arr,
+  algorithm,
+}: {
+  arr: Array<number>;
+  algorithm: any;
+}) => {
   const [array, setArray] = useState<number[]>(arr);
   const [changedIndices, setChangedIndices] = useState<number[]>(arr);
+  const arrayRef = useRef<any>();
 
-  // function iterateGenerator(generator: Generator) {
-  //   const { done, value } = generator.next();
+  const updateArray = (updatedArray: any) => {
+    setTimeout(() => {
+      console.log(updatedArray), 2000;
+    });
+  };
 
-  //   if (!done) {
-  //     if (typeof value !== "string") {
-  //       const arrayValue = value as number[];
-  //       console.log("Swapping", value);
-  //       setArray(arrayValue);
-
-  //     }
-  //     iterateGenerator(generator);
-  //   } else {
-  //     setArray(array);
-  //   }
-  // }
-
-  // useEffect(() => {
-
-  //   const sorter = algorithm(arr);
-  //   iterateGenerator(sorter);
-  // }, []);
+  let i = 1;
   useEffect(() => {
     const sorter = algorithm(arr.slice()); // Create a copy for sorting
 
     const iterateGenerator = (generator: Generator) => {
       const { done, value } = generator.next();
 
-      const { array, swap }: { array: number[]; swap: number[] } = value as { array: number[]; swap: number[] };
+      const { stepArray, swap }: { stepArray: number[]; swap: number[] } =
+        value as { stepArray: number[]; swap: number[] };
 
       if (!done) {
         if (swap.length > 1) {
-          console.log(value)
-          setArray(array);
+          // const updatedArray = stepArray
+          // setArray(prevState => {
+
+          //   console.log("Swap" , swap)
+          //   console.log("Previous state",  prevState);
+
+          //   [prevState[swap[0]], prevState[swap[1]]] = [prevState[swap[1]], prevState[swap[0]]]
+          //   console.log("Updated state",  prevState)
+          //   console.log("Step Array" , stepArray);
+          //   return prevState
+          // });
+          console.log(stepArray);
+          // console.log(arrayRef.current.children);
+          {
+            i++;
+            setTimeout(() => {
+              console.log("swapped");
+              console.log(arrayRef.current.children[swap[0]])
+              arrayRef.current.children[swap[0]].style.backgroundColor = "rgb(244 114 182)";
+              arrayRef.current.children[swap[1]].style.backgroundColor = "rgb(34 211 238)";
+              [
+                arrayRef.current.children[swap[0]].innerText,
+                arrayRef.current.children[swap[1]].innerText,
+              ] = [
+                arrayRef.current.children[swap[1]].innerText,
+                arrayRef.current.children[swap[0]].innerText,
+              ];
+
+              // arrayRef.current.children[swap[0]].style.color = "black";
+              // arrayRef.current.children[swap[1]].style.color = "black";
+
+            }, i * 2000);
+          } // setArray(() => {
+          //   return stepArray})
+          // updateArray(updatedArray)
           setChangedIndices(swap);
           iterateGenerator(generator);
         } else {
@@ -49,56 +77,34 @@ const NumberArray = ({ arr, algorithm }: { arr: Array<number>; algorithm: any })
     iterateGenerator(sorter);
   }, []);
 
-  // useEffect(() => {
-    // const previousArray = previousArrayRef.current;
-    // const changedIndices = [];
-
-    // for (let i = 0; i < array.length; i++) {
-    //   if (array[i] !== previousArray[i]) {
-    //     changedIndices.push(i);
-    //   }
-    // }
-
-    // previousArrayRef.current = array.slice();
-
-    // Trigger rerender with animation
-  //   changedIndices.forEach((index) => {
-  //     // Apply animation to the element at index
-  //     const element = document.getElementById(`arrayElement-${index}`);
-  //     if (element) {
-  //       element.classList.add("animate-swap");
-  //       setTimeout(() => {
-  //         element.classList.remove("animate-swap");
-  //       }, 500); // Adjust animation duration as needed
-  //     }
-  //   });
-  // }, [array]);
-
-  // const animationPromise = new Promise((resolve) => {
-  //   const element = document.getElementById('yourElementId');
-  //   element?.addEventListener('animationend', resolve);
-  // });
-
   return (
-    <div style={{ display: "flex" }}>
-      {array.map((element: number, idx: number) => (
-        <div
-          className="arrayElement"
-          id={`arrayElement-${idx}`}
-          key={idx}
-          style={{
-            height: "40px",
-            width: "40px",
-            border: "1px solid black",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {element}
-        </div>
-      ))}
-    </div>
+    <>
+      {/* <motion.div
+        style={{ backgroundColor: "#000", height: "200px", width: "200px" }}
+        className="box"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+      /> */}
+      <div style={{ display: "flex" }} ref={arrayRef}>
+        {array.map((element: number, idx: number) => (
+          <div
+            className="arrayElement"
+            id={`arrayElement-${idx}`}
+            key={idx}
+            style={{
+              height: "40px",
+              width: "40px",
+              border: "1px solid black",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {element}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
