78b2533efb841d8fcd2a2f82b5a91112bfee78dd
anushkadeshpande <anushkaa5000@gmail.com>
2024-10-20 01:55:51 +0530

    feat: Added all required constants

---
+++ src/utils/constants.d.ts
@@ -0,0 +1,6 @@
+declare const ARRAY : string
+declare const LINKED_LIST : string
+declare const GRAPH : string
+declare const BINARY_TREE : string
+declare const SORTING : string
+declare const SEARCHING : string
---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const ARRAY = "Array"
+export const LINKED_LIST = "Linked List"
+export const GRAPH = "Graph"
+export const BINARY_TREE = "Binary Tree"
+export const SORTING = "Sorting"
+export const BUBBLE_SORT = "Bubble Sort"
+export const QUICK_SORT = "Quick Sort"
+
+export const SEARCHING = "Searching"
+export const BINARY_SEARCH = "Binary Search"
