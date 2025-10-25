59d140f2339a25b5ef39cb9c8d93c4c7177bbd2f
anushkadeshpande <anushkaa5000@gmail.com>
2025-10-25 18:37:41 +0530

    feat: Passed eventArr as ref instead of creating a new array everytime

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import SideBar from './components/SideBar'
-import BubbleSort from './BubbleSort'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import MainContent from './components/MainContent'
 // import Canvas from './Canvas'
 // import Array from './components/NumberArray'
@@ -9,11 +8,17 @@ import MainContent from './components/MainContent'
 
 function App() {
   const [view, setView] = useState("")
+  // useEffect(() => {
+  //   clearTimeout(event);
+  // }, [view])
+  const eventArrRef = useRef<Array<ReturnType<typeof setTimeout>>>([])
+  // Keep a stable timeout registry so NumberArray can cancel prior animations when view changes.
+  const eventArr = eventArrRef.current
   return (
     <div className='h-screen w-screen bg-gray-900 flex'>
 
       <SideBar view = {view} setView = {setView}/>
-      <MainContent view = {view} setView = {setView}/>
+      <MainContent view = {view} setView = {setView} eventArr={eventArr}/>
     {/* <Array arr={[1, 2, 3, 4, 5, 100, 3, 4, 5, 1, 3]}/> */}
     {/* {bubbleSort()} */}
     {/* <BubbleSort arr={[1, 2, 3, 4, 5, 100, 3, 4, 5, 1, 3]} /> */}
